3dc0a2533a0211bbb1b8dc54d7a5a0380ad2094a
gitwithsunil <reddysu81@gmail.com>
2026-05-19 23:31:09 +0530

    Add initial files

---
+++ 00-variable.js
@@ -0,0 +1,330 @@
+
+// var x = "sunil(656)"
+// console.log(x)
+// console.log(typeof(x))
+
+// let age = 23
+// console.log(age)
+// console.log(typeof(age))
+
+// let mySalary
+// console.log(mySalary)//undeifned
+// console.log(typeof(mySalary))//undefined 
+
+// let y = Symbol("hello")
+// console.log(y)
+// console.log(y.description)
+// console.log(typeof(y))
+
+// let arr = [1 ,2 ,3 ,4 ,5 ,6]
+// console.log(arr)
+// console.log(arr[3])//4
+// console.log(arr[5])//6
+// console.log(typeof(arr))//object
+
+
+//----object-->>>
+
+// let student = {
+//     "name" : "sunil",
+//     "age" : "22",
+//     "roll" : "117",
+//     "carrier" : "good"
+
+// }
+// console.log(student)
+// console.log(typeof(student))
+// console.log(student['age'])
+// console.log(student['carrier'])
+
+//-----function--->>>
+
+
+// function mySced(){
+//     console.log("i wake up at 3 a.m.")
+//     console.log("i eat Biryani")
+
+// }
+
+// mySced()
+// mySced()
+// mySced()
+
+//---operator---->>
+
+// let a = 32
+// let b = 34
+// console.log(a+b)
+// console.log(a-b)
+// console.log(a*b)
+// console.log(a/b)
+// console.log(a%b)
+// console.log(a**b)//'a' to the power 'b'
+
+
+
+//unary operator
+// let a = 7
+// let b = 8
+
+// console.log(a)
+// a++;
+// console.log(a)//8
+// a--;
+// console.log(a)//7
+// a--;
+// console.log(a)//6
+
+//assignment operator
+
+// let a = 7
+// console.log(a)
+// a += 5; //a = a+5
+// console.log(a)//12
+// a -= 5; //a = a-5
+// a*=2;
+// console.log(a)
+// a/=2;
+// console.log(a)
+// a%=2;
+// console.log(a)
+// a**=2;
+// console.log(a)
+
+//comparison 
+// console.log(2==2)
+// console.log(3!=3)
+// console.log(2 === '2')//strictly equal to (false AS '2' is string)
+// console.log(2!=='2')
+// console.log(2>2)
+// console.log(3>=3)
+// console.log(3<=3)
+
+//logical--->
+// let x = 5
+// console.log((x<6) && (x>4))
+// console.log((x<4) || (x>4))
+// console.log(!(x==2))
+// console.log(!(x<2))
+
+
+//--conditional statement---------------------------------------------------->>>
+
+// let age = 15
+// if(age>=18){
+//     console.log("the student is adult")
+// } else {
+//     console.log("not a adult")
+// }
+
+// let mode = "red"
+// let color;
+// if ( mode == "dark"){
+//     color = "black"
+
+// } else if (mode == "blue"){
+//     color = "blue"
+// } else if (mode == "red"){
+//     color = "red"
+// } else {
+//     color = "cannot find the color"
+// }
+
+// console.log(color)
+
+// //write a program to find grades of the srudents according to the marks obtained
+//90-100-->> A
+//80-89-->> B
+//70-79-->> C
+//60-69-->> D
+//0-59-->> F    
+
+// let marks = 85
+// let grade;
+// if(marks>=90 && marks<=100){
+//     grade = "A"
+// }else if(marks>=80 && marks<=89){
+//     grade = "B"
+// }else if (marks>=70 && marks<=79){
+//     grade = "C"
+// } else if (marks>=60 && marks<=69){
+//     grade = "D"
+// } else if (marks>=0 && marks<=59){
+//     grade = "F"
+// }   else {
+//     grade = "invalid marks"
+// }       
+// console.log(grade)
+
+// --using loops---------------------------------------->>>
+
+//for loop
+//while loop
+//do while loop 
+// for-of loop
+// for-in loop
+
+// let string = "web-devlopement"
+// for (let i = 0; i<10 ; i++){
+//     console.log("webdev")
+// }
+// // calculate the sum of 1 to 10
+
+// let sum = 0;
+// for (let i = 1;i<=10;i++){
+//     sum += i;
+// }
+// console.log(sum)
+
+// // print javascript 5 times using while loop
+
+// let string = "javascript"
+// let i=0;
+// while (i<5){
+//     console.log(string)
+//     i++;        
+// } 
+
+
+// // print WBC 5 times using do while loop
+
+// let string = "WBC"
+// let i = 0;
+// do {
+//     console.log(string)
+//     i++;
+
+// } while (i<5)       
+
+// print the string java script using for of loop
+
+// let string = "java script"
+// for(let i of string){
+//     console.log(i)
+// }
+
+// let arr = [1,2,3,4,5]
+// for (let i of arr){
+//     console.log(i)
+    
+// }
+// console.log(arr.length)
+
+// // print the index of the array using for in loop
+
+// let arr = [1,2,3,4,5,6,7]
+// let s = 0;
+// for (let i in arr){
+//     s += arr[i]
+// }
+
+// console.log(s)  
+
+// // print object using for in loop also print the keys and values of the object
+
+// let student = {
+//     "name" : "sunil",
+//     "age" : "22",
+//     "roll" : "117",
+//     "carrier" : "good"  
+// }
+// for (let key in student){
+//     console.log("key =",key, "value =",student[key])
+// }
+
+// -----String ----------------------------------------------->>>
+
+// let string = "web-devlopement"
+// console.log(string[3])
+// console.log(string[4][2])//undefined as string[4] is 'd' and 'd'[2] is undefined
+
+// // write a program to print the particular like add 0 to 4 pr address 3 to 7 of a string  length of the string
+
+// let string = "web-devlopement"
+// console.log(string.length)
+// console.log(string.slice(0,5))//web-d
+// console.log(string.slice(3,8))//-devl
+// console.log(string.slice(0,15))//web-devlopement    
+  
+// let string = "web-devlopement"
+// console.log(string.toUpperCase())//WEB-DEVLOPEMENT
+// console.log(string.toLowerCase())//web-devlopement
+// console.log(string.trim())//web-devlopement
+
+
+// i want to create a web page where i want to show the name of the student in uppercase and the name of the college in lowercase and also remove the extra spaces from the string
+
+// let studentName = "sunil"   
+// let collegeName = "  ABC college  "
+// console.log(studentName.toUpperCase())//SUNIL
+// console.log(collegeName.toLowerCase())//  abc college  
+// console.log(collegeName.trim())//abc college
+
+// examlpes 
+
+// let object = {
+//     name : "sunil",
+//     age : 22,
+//     college : "NIST UNIVERSITY",
+//     roll : 117
+
+// }
+// let output = `my name is ${object.name} and i am ${object.age} years old. i am studying in ${object.college} and my rolll number is ${object.roll}`
+// console.log(output)
+// let string ="hello world\nwelcome to javascript\tprogramming"
+
+// console.log(string)
+// console.log(string.length)  // \n is used for new line and \t is used for tab space so no chamnge in the length of the string as they are escape characters and not visible in the output but they are counted in the length of the string.
+// demo = string.toUpperCase()
+// console.log(string)
+// console.log(demo)
+// trim() method is used to remove the extra spaces from the string
+// let str = "   hello world   "
+// console.log(str)
+// console.log(str.trim())//hello world,  all the extra spaces are removed from the string but the spaces between the words are not removed as they are not extra spaces.
+
+// let str1 = "helllo"
+// let str2 = "world"
+// let str3 = str1.concat(str2)
+// console.log(str3)
+
+// replace() method is used to replace a part of the string with another string
+
+// let str1 = "hello world"
+// let str2 = str1.replace("world","javascript")
+// console.log(str2)
+// console.log(str2.replace("javascript","python"))
+// console.log(str1.replaceAll("l","L"))//heLLo worLd, replaceAll() method is used to replace all the occurrences of a character in the string with another character.
+// console.log(str1.replaceAll("o","O"))//hellO wOrld, replaceAll() method is used to replace all the occurrences of a character in the string with another character.
+
+// charAt() method is used to get the character at a specific index of the string
+
+// let str = "hello world"
+// console.log(str.charAt(3))//l, charAt() method is used to get the character at a specific index of the string. index starts from 0, so charAt(3) will return the character at index 3 which is 'l' in this case.    
+// console.log(str.charAt(9))//l
+// console.log(str.charAt(0))//h
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ 07-array.js
@@ -0,0 +1,121 @@
+// let arr = [1,2,3,4,5,6,7]
+// let s = 0;
+// for (let i in arr){
+//     s = s + arr[i]// s += arr[i]
+// }  
+// console.log(s)
+
+// // for a given array marks of the student -> [80,90,70,60,85] , find the average marks of the student-----Question
+
+// let marks = [80,90,70,60,85]
+// let sum = 0;
+// for ( let i in marks){
+//     sum += marks[i]
+// }
+// let ave = sum/marks.length
+// console.log("Average marks:", ave)
+
+
+// // for a given array with price of 5 products -> [100,200,150,300,250] , all items have 10% off , change the price of the products after discount and print the new price of the products
+
+// let price = [100,200,150,300,250]
+// for (let i in price){
+//     price[i] = price[i] - (price[i] * 0.1) // price[i] = price[i] - (price[i] * 10/100)
+// }
+// console.log("price after discount: ", price)
+
+// Array methods in js:-
+
+// 1. push() -> adds an element at the end of the array
+// 2. pop() -> removes the last element from the array
+// 3. shift() -> removes the first element from the array  
+// 4. unshift() -> adds an element at the beginning of the array
+// 5. indexOf() -> returns the index of the first occurrence of a specified value in the array, or -1 if it is not found.
+// 6. includes() -> determines whether an array includes a certain value among its entries, returning true or false as appropriate.    
+// 3.toString() -> converts an array to a string, and returns the result. The elements will be separated by commas.
+// 4.join() -> joins all elements of an array into a string and returns this string. You can specify a separator as an argument to join(), which will be used to separate the elements in the resulting string. If no separator is provided, the default separator is a comma (,).
+// 5.concat() -> is used to merge two or more arrays. This method does not change the existing arrays, but instead returns a new array.
+// 6.slice() -> returns a shallow copy of a portion of an array into a new array object selected from start to end (end not included). The original array will not be modified.
+// 7.splice() -> changes the contents of an array by removing or replacing existing elements and/or adding new elements in place. It modifies the original array and returns an array containing the removed elements (if any).
+// 8.sort() -> sorts the elements of an array in place and returns the sorted array. The default sort order is ascending, built upon converting the elements into strings, then comparing their sequences of UTF-16 code units values.
+// 9.reverse() -> reverses the order of the elements in an array in place. The first array element becomes the last, and the last array element becomes the first. This method modifies the original array and returns a reference to the same array.
+// 10.map() -> creates a new array populated with the results of calling a provided function on every element in the calling array. It does not modify the original array.
+// 11.filter() -> creates a new array with all elements that pass the test implemented by the provided function. It does not modify the original array.
+// 12.reduce() -> executes a reducer function (that you provide) on each element of the array, resulting in a single output value. It does not modify the original array.
+
+//examples
+
+// let marks = [80,90,70,60,85]
+// console.log(marks)
+// console.log(marks.length)
+// marks.push(95) // adds 95 at the end of the array
+// console.log(marks[2])// prints the element at index 2 -> 70
+// marks[2] = 75 // changes the element at index 2 to 75
+// console.log(marks)
+
+// console.log(marks.pop())
+
+// let marks = [80,90,70,60,85]
+// console.log(marks)
+// let demo = marks.toString()
+// console.log(demo)
+
+// let marvelHeroes = ["Iron Man", "Captain America", "Thor", "Hulk", "Black Widow"]
+// let dcHeroes = ["Superman", "Batman", "Wonder Woman", "Flash", "Aquaman"]
+
+// let allHeroes = marvelHeroes.concat(dcHeroes)
+// console.log(allHeroes)
+
+//slice() example
+
+// let animals = ["dog","cat","elephant","tiger","lion"]
+// let newAnimals = animals.slice(1,4) // it will return a new array with elements from index 1 to index 3 (4 is not included)
+// console.log(newAnimals) // ["cat","elephant","tiger"]
+// console.log(animals) // original array is not modified
+// console.log(animals.slice(2)) // it will return a new array with elements from index 2 to the end of the array -> ["elephant","tiger","lion"]
+// console.log(animals.slice(-2)) // it will return a new array with the last 2 elements of the array -> ["tiger","lion"]
+// console.log(animals.slice(2,-1))// it will return a new array with elements from index 2 to index -1 (last element is not included) -> ["elephant","tiger"]
+
+// //splice() example
+
+// let months = ["Jan","Feb","Apr","May","Jun"]
+// months.splice(2,0,"Mar") // it will add "Mar" at index 2 without removing any element
+// console.log(months) // ["Jan","Feb","Mar","Apr","May","Jun"]
+// console.log(months.splice(5,1,"jul")) // it will replace "Jun" with "Jul" at index 5 and return the removed element -> ["Jun"]
+// console.log(months) // ["Jan","Feb","Mar","Apr","May","Jul"]
+// console.log(months.splice(3,1))// it will remove "Apr" from index 3 and return the removed element -> ["Apr"]
+// console.log(months) // ["Jan","Feb","Mar","May","Jul"] it will remove "Apr" from index 3 and return the removed element -> ["Apr"]
+
+//------------FUNCTION in JS------------------------------------------------------------->>>>
+
+// it is a block of code that performs a specific task and can be reused multiple times in a program. Functions are defined using the function keyword, followed by the function name, parentheses, and curly braces. The code to be executed is placed inside the curly braces.
+
+// function greet(){
+//     console.log("Hello, welcome to JavaScript!")
+// }
+// greet() // calling the function to execute the code inside it    
+
+//---------------------example --->>>
+
+// function add(a,b){
+//     return a+b
+// }
+// console.log("Addition:",add(5,1))
+
+// arrow function in js -> it is a shorter syntax for writing functions in JavaScript. It is defined using the arrow (=>) syntax and does not have its own this, arguments, super, or new.target keywords. Arrow functions are always anonymous and cannot be used as constructors.
+
+// let add = (a,b) => {
+//     return a+b
+// }
+// console.log("Addition:",add(5,1))        
+
+// let sum = (a,b) =>{
+//     return a+b
+// }
+// console.log("Sum:",sum(5,10))
+
+// 1.event/event handler -> it is an action or occurrence that happens in the system you are programming, which the system tells you about so your code can respond to it. An event handler is a function that is called when an event occurs. It is used to handle events such as user interactions (clicks, key presses, etc.) or system events (loading, resizing, etc.). Event handlers are typically registered using methods like addEventListener() in JavaScript.
+//2async & wait -> async is a keyword used to declare an asynchronous function, which is a function that can perform asynchronous operations and return a promise. The await keyword is used inside an async function to pause the execution of the function until a promise is resolved. This allows you to write asynchronous code in a more synchronous and readable manner. When the await keyword is used, the function will wait for the promise to resolve before continuing with the next line of code.
+
+
+
