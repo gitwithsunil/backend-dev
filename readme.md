b5b01a29c64d1aba80ccb6cbd0cc1765972507e5
gitwithsunil <reddysu81@gmail.com>
2026-05-22 12:01:10 +0530

    status code update

--- readme.md
+++ readme.md
@@ -3,3 +3,20 @@
 ## 00-variable.js file contains all the basics 
 ## 07-array.js file contains as named 
 ## 08-functions.js 
+
+
+STAUS CODE ->
+
+- 200 -> ok
+- 201 -> created
+- 202 -> accepted
+- 300 -> multiple choice 
+- 302 -> found
+- 400 -> bad request
+- 402 -> payment required
+- 404 -> not found
+- 406 -> not accepted 
+- 408 -> request timeout
+- 500 -> internal server error 
+- 502 -> bad gateway
+- 504 -> gateway timeout
--- script.js
+++ script.js
@@ -1,7 +1,16 @@
-let btn = document.querrySelector("#btn")
-btn.oneclick = () => {
-    console.log("btn was clicked 1 time")
+// Selecting first button
+let btn = document.querySelector("#btn");
 
-}
+// Single click event
+btn.onclick = () => {
+    console.log("btn was clicked 1 time");
+};
 
-let btn1 = document.querrySelector
+
+// Selecting second button
+let btn1 = document.querySelector("#btn1");
+
+// Double click event
+btn1.ondblclick = () => {
+    console.log("btn was clicked 2 times");
+};
