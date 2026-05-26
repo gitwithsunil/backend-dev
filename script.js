389fab54c2d568c038bc96b9cca0b1c8eb45312f
gitwithsunil <reddysu81@gmail.com>
2026-05-26 11:25:28 +0530

    new updatein same file

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
-function sum(a,b){
+function sum(a,b){ // normal function
     console.log(a+b);
 }
-function calc(a,b,sumCallback){
+function calc(a,b,sumCallback){ // main/ callback function
     sumCallback(a,b)
 }
-calc(1,2,sum)
+calc(1,2,sum) // function passed as an argument to another function
+
