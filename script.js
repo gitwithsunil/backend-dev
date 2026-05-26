c034a25e1a6ed12917ab3f32be80b12d1c56cfb4
gitwithsunil <reddysu81@gmail.com>
2026-05-26 11:36:28 +0530

    update

--- script.js
+++ script.js
@@ -1,8 +1,35 @@
-function sum(a,b){ // normal function
-    console.log(a+b);
-}
-function calc(a,b,sumCallback){ // main/ callback function
-    sumCallback(a,b)
-}
-calc(1,2,sum) // function passed as an argument to another function
+// function sum(a,b){ // normal function
+//     console.log(a+b);
+// }
+// function calc(a,b,sumCallback){ // main/ callback function
+//     sumCallback(a,b)
+// }
+// calc(1,2,sum) // function passed as an argument to another function
 
+// ------
+function getData(dataID , generation){
+    setTimeout(() => {
+        console.log("data", dataID)
+        if (getNextData){
+            getNextData():
+        }
+    }, 2000)
+}
+getData(1)
+
+//callback hell ->>
+
+console.log("getting data-1.....")
+getData(1, () => { 
+    console.log("getting data-2.....")
+    getData(2, () => {
+        console.log("getting data-3.....")
+        getData(3, () => {
+            console.log("getting data-4.....")
+            getData(4, () => {
+                console.log("getting data-5.....")  
+                getData(5)
+
+        })
+    })
+})
